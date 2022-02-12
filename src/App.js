405503ae5af4abfa8b5071fb31f856c0629e71a1
Nazanin Hashemian <69569381+NazaninHashemian@users.noreply.github.com>
2022-02-12 01:29:12 -0800

    My first commit

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React,{Component} from 'react';
+import NavBar from './components/navbar';
+import Counters from './components/counters';
+//import Component from './components/counters';
+import './App.css';
+//import reactDom from 'react-dom';
+//import Counter from './components/counter';
+
+
+class App extends Component {
+  state={
+    counters:[
+        { id: 1, value:4},
+        { id: 2, value:0},
+        { id: 3, value:0},
+        { id: 4, value:0}
+    ]
+};
+
+constructor() {
+  super();
+  console.log('App - Constructor');
+}
+
+componentDidMount() {
+  console.log('App - Mounted: right place for make Ajax call & get data from the server');
+}
+
+handleIncrement =counter => {
+    const counters=[...this.state.counters];
+    const index= counters.indexOf(counter);
+    counters[index] = {...counter};
+    counters[index].value++;
+    this.setState({counters});
+}
+
+handelReset = () =>{
+    const counters = this.state.counters.map(c => {
+        c.value = 0;
+        return c;
+    });
+    this.setState({counters});
+};
+
+handelDelete = counterId=> {
+    const counters =this.state.counters.filter(c => c.id !== counterId);
+    this.setState({counters});
+};
+
+handleDecrement =counter => {
+  const counters=[...this.state.counters];
+  const index= counters.indexOf(counter);
+  counters[index] = {...counter};
+  counters[index].value--;
+  this.setState({counters});
+}
+
+  render(){
+console.log('App - Rendered');
+
+    return(
+      <React.Fragment>
+        <NavBar 
+          totalCounters={this.state.counters.filter(c => c.value > 0).length}
+        />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handelReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handelDelete}
+            onDecrement={this.handleDecrement}
+            />
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/common/like.jsx
@@ -0,0 +1,9 @@
+import React, {component} from 'react';
+
+class Like extends component{
+    render() {
+        return <i class="fa fa-heart-o" aria-hidden="true"></i> ;
+    }
+}
+
+export default Like;
